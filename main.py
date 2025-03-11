a5eb0a586ad3102dc903a2ed273c4a8cc0eab49e
tangyorigami <csaenz613@gmail.com>
2025-03-11 15:55:26 -0500

    idk what i did

--- main.py
+++ main.py
@@ -4,16 +4,18 @@ from datetime import datetime
 
 workbook = xlrd.open_workbook('MAIN OFFICE DETAIL PAYROLL REPORT.xls')
 
-control = 8
+weekdays = "[Thu|Fri|Mon|Tue|Wed]"
+weekends = "[Sat|Sun]"
 
 
 class LoggedUserHrs:
     def __init__(self, name, report):
         self.name = str(name)
         self.report = report
+        self.hours = self.__get_hrs_wrked()
         self.date = datetime.now().strftime("%m-%d-%Y")
 
-    def __getHrs(self):
+    def __get_hrs(self):
         '''
             Returns the Hours that were clocked in by date.
         '''
@@ -23,7 +25,7 @@ class LoggedUserHrs:
                 hours.append(self.report[i][1])
         return hours
 
-    def __getDates(self):
+    def __get_dates(self):
         '''
             Returns the Dates where hours were clocked in.
         '''
@@ -33,7 +35,7 @@ class LoggedUserHrs:
                 dates.append(self.report[i][0])
         return dates
 
-    def getHrsWrked(self):
+    def __get_hrs_wrked(self):
         '''
             Returns a Dictionary with the date hours were
             logged as the Key and the hours as the Value.
@@ -42,22 +44,12 @@ class LoggedUserHrs:
                 {'Thu 1/30' : 8}
         '''
         weekHrs = dict()
-        dates = self.__getDates()
-        hrs = self.__getHrs()
+        dates = self.__get_dates()
+        hrs = self.__get_hrs()
         for i in range(len(dates)):
             weekHrs.update({dates[i]: hrs[i]})
 
         return weekHrs
-
-
-def dailyHrs(sheet, control):
-    for i in range(32, sheet.nrows):
-        if sheet.cell_value(rowx=i, colx=25) == "":
-            pass
-        else:
-            print("{0}, 25, \"{1}\", \"{2}\"".format(
-                i, sheet.cell_value(rowx=i, colx=25), control))
-            control = + 8
 
 
 def hrsFormatter(arr):
@@ -108,12 +100,13 @@ for i in range(0, workbook.nsheets):
     if len(currUser) != 1:
         users.append(LoggedUserHrs(currUser[0], currUser[1:]))
 
+gp = users[0]
 
-print("{0}".format(
-    users[2].getHrsWrked()
+print("{0}\n".format(
+    sum(gp.hours.values())
 ))
 
-for i in range(len(users)):
-    print("{0},".format(
-        users[i].getHrsWrked()
+for i in gp.hours.keys():
+    print("'{0}', {1},".format(
+        i, gp.hours[i]
     ))
