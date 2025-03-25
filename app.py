d795da15f737bfd8a34339e95002c5f9abfa10b2
tangyorigami <csaenz613@gmail.com>
2025-03-25 16:08:17 -0500

    working on ui

--- app.py
+++ app.py
@@ -1,146 +1,63 @@
 import sys
 from PyQt6.QtWidgets import (
-    QApplication, QWidget, QPushButton, QFileDialog,
-    QVBoxLayout, QLabel, QTableWidget, QTableWidgetItem,
-    QMainWindow, QMenu
+    QApplication, QMainWindow, QFileDialog, QVBoxLayout
 )
 
-from PyQt6.QtCore import Qt
-from PyQt6.QtGui import QDragEnterEvent, QDropEvent, QAction
-from util.controller import Controller as c
+from PyQt6.QtGui import QAction
+from ui.fileupload import FileUploadWidget
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.show()
 
-        self.setContextMenuPolicy(Qt.ContextMenuPolicy.CustomContextMenu)
-        self.customContextMenuRequested.connect(self.contextMenuEvent)
+        self.setWindowTitle("My App")
 
-    def contextMenuEvent(self, pos):
-        context = QMenu(self)
-        context.addAction(QAction("test 1", self))
-        context.addAction(QAction("test 2", self))
-        context.addAction(QAction("test 3", self))
-        context.exec(self.mapToGlobal(pos))
+        self.file_upload_widget = FileUploadWidget()
 
+        # Menu
+        open_button = QAction("Open", self)
+        open_button.setStatusTip("Open a file")
+        open_button.triggered.connect(self.file_upload_widget.open_file_dialog)
 
-class FileUploadWidget(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setAcceptDrops(True)  # Enable drag and drop
+        save_button = QAction("Save", self)
+        save_button.setStatusTip("Save current view")
+        save_button.triggered.connect(self.save_button_action)
 
-        # Title Label
-        self.title_label = QLabel("Excel File Parser", self)
-        self.title_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.title_label.setStyleSheet(
-            "font-size: 20px; font-weight: bold; padding: 10px;")
+        save_as_button = QAction("Save as", self)
+        save_as_button.setStatusTip("Save current view as")
+        save_as_button.triggered.connect(self.save_as_button_action)
 
-        # File Upload Button
-        self.upload_button = QPushButton("Upload File")
-        self.upload_button.clicked.connect(self.open_file_dialog)
-        self.upload_button.setStyleSheet("padding: 8px; font-size: 14px;")
+        close_button = QAction("Close", self)
+        close_button.setStatusTip("Close current view")
+        close_button.triggered.connect(self.close)
 
-        # File Path Label
-        self.file_path_label = QLabel(
-            "Drag and drop a file here or use the button to upload.", self)
-        self.file_path_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        self.file_path_label.setStyleSheet(
-            "color: gray; font-style: italic; padding: 10px;")
+        menu = self.menuBar()
 
-        # Table Widget for Displaying Data
-        self.table_widget = QTableWidget()
-        self.table_widget.setColumnCount(0)  # Will be set after parsing
-        self.table_widget.setStyleSheet("border: 1px solid gray;")
-        self.table_widget.setEditTriggers(
-            QTableWidget.EditTrigger.NoEditTriggers)
+        file_menu = menu.addMenu("&File")
+        file_submenu = file_menu.addMenu("Save")
+        file_submenu.addAction(save_button)
+        file_submenu.addSeparator()
+        file_submenu.addAction(save_as_button)
+        file_menu.addSeparator()
 
-        # Status Label
-        self.status_label = QLabel("")
-        self.status_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        file_menu.addAction(open_button)
+        file_menu.addSeparator()
 
-        # Layouts
-        top_layout = QVBoxLayout()
-        top_layout.addWidget(self.title_label)
+        file_menu.addAction(close_button)
 
-        middle_layout = QVBoxLayout()
-        middle_layout.addWidget(self.upload_button)
-        middle_layout.addWidget(self.file_path_label)
+        self.setCentralWidget(self.file_upload_widget)
 
-        bottom_layout = QVBoxLayout()
-        bottom_layout.addWidget(self.table_widget)
-        bottom_layout.addWidget(self.status_label)
-
-        # Main Layout
-        main_layout = QVBoxLayout()
-        main_layout.addLayout(top_layout)
-        main_layout.addLayout(middle_layout)
-        main_layout.addLayout(bottom_layout)
-
-        self.setLayout(main_layout)
-
-    def open_file_dialog(self):
+    def open_button_action(self, s):
         file_path, _ = QFileDialog.getOpenFileName(
             self, "Select Excel File", "", "Excel Files (*.xls)")
-        if file_path:
-            self.process_file(file_path)
+        self.process_file(file_path)
 
-    def process_file(self, file_path):
-        self.file_path_label.setText(f"Selected file: {file_path}")
-        self.status_label.setText("Processing file...")
+    def save_button_action(self, s):
+        print("save", s)
 
-        try:
-            users = c.extract_data(file_path)
-            self.populate_table(users)
-            self.status_label.setText("File successfully processed!")
-        except Exception as e:
-            self.status_label.setText(f"Error processing file: {e}")
-
-    def populate_table(self, users):
-        if not users:
-            self.status_label.setText("No data found in file.")
-            return
-
-        pay_period = users[0].pay_period_dates()
-        headers = ["Name", "Start Date"] + pay_period
-
-        self.table_widget.setColumnCount(len(headers))
-        self.table_widget.setRowCount(len(users))
-        self.table_widget.setHorizontalHeaderLabels(headers)
-
-        for row_id in range(0, len(users)):
-            user = users[row_id]
-            user_info = {"Name": user.name, "Start Date": user.start_date}
-            user_data = {**user_info, **user.get_hrs_wrked()}
-
-            for col_id in range(len(headers)):
-                if headers[col_id] in user_data.keys():
-                    value = str(user_data[headers[col_id]])
-
-                    self.__add_cell_value(
-                        row_id=row_id, col_id=col_id, value=value)
-
-    def __add_cell_value(self, row_id, col_id, value):
-        item = QTableWidgetItem(
-            str(value))
-        self.table_widget.setItem(
-            row_id, col_id, item)
-
-    # Drag & Drop Events
-
-    def dragEnterEvent(self, event: QDragEnterEvent):
-        if event.mimeData().hasUrls():
-            event.acceptProposedAction()
-
-    def dropEvent(self, event: QDropEvent):
-        for url in event.mimeData().urls():
-            file_path = url.toLocalFile()
-            if file_path.endswith((".xls")):
-                self.process_file(file_path)
-            else:
-                self.status_label.setText(
-                    "Invalid file type. Please select an Excel file.")
+    def save_as_button_action(self, s):
+        print("save as", s)
 
 
 if __name__ == '__main__':
---
+++ ui/fileupload.py
@@ -0,0 +1,80 @@
+from PyQt6.QtWidgets import (
+    QWidget, QFileDialog, QVBoxLayout, QLabel,
+    QTableWidget, QTableWidgetItem
+)
+
+from PyQt6.QtCore import Qt
+from util.controller import Controller as c
+
+
+class FileUploadWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setAcceptDrops(True)  # Enable drag and drop
+
+        # Table Widget for Displaying Data
+        self.table_widget = QTableWidget()
+        self.table_widget.setColumnCount(0)  # Will be set after parsing
+        self.table_widget.setStyleSheet("border: 1px solid gray;")
+        self.table_widget.setEditTriggers(
+            QTableWidget.EditTrigger.NoEditTriggers)
+
+        # Status Label
+        self.status_label = QLabel("")
+        self.status_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+
+        bottom_layout = QVBoxLayout()
+        bottom_layout.addWidget(self.table_widget)
+        bottom_layout.addWidget(self.status_label)
+
+        # Main Layout
+        main_layout = QVBoxLayout(self)
+        main_layout.addLayout(bottom_layout)
+
+        self.setLayout(main_layout)
+
+    def open_file_dialog(self):
+        file_path, _ = QFileDialog.getOpenFileName(
+            self, "Select Excel File", "", "Excel Files (*.xls)")
+        if file_path:
+            self.process_file(file_path)
+
+    def process_file(self, file_path):
+        self.status_label.setText("Processing file...")
+
+        try:
+            users = c.extract_data(file_path)
+            self.populate_table(users)
+            self.status_label.setText("File successfully processed!")
+        except Exception as e:
+            self.status_label.setText(f"Error processing file: {e}")
+
+    def populate_table(self, users):
+        if not users:
+            self.status_label.setText("No data found in file.")
+            return
+
+        pay_period = users[0].pay_period_dates()
+        headers = ["Name", "Start Date"] + pay_period
+
+        self.table_widget.setColumnCount(len(headers))
+        self.table_widget.setRowCount(len(users))
+        self.table_widget.setHorizontalHeaderLabels(headers)
+
+        for row_id in range(0, len(users)):
+            user = users[row_id]
+            user_info = {"Name": user.name, "Start Date": user.start_date}
+            user_data = {**user_info, **user.get_hrs_wrked()}
+
+            for col_id in range(len(headers)):
+                if headers[col_id] in user_data.keys():
+                    value = str(user_data[headers[col_id]])
+
+                    self.__add_cell_value(
+                        row_id=row_id, col_id=col_id, value=value)
+
+    def __add_cell_value(self, row_id, col_id, value):
+        item = QTableWidgetItem(
+            str(value))
+        self.table_widget.setItem(
+            row_id, col_id, item)
